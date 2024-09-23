f605ef3af56f73d51dc5c8227906bcd055c426ca
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-22 23:42:45 -0300

    refactor: map method SeriesController.getSeries() to route /series

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
@@ -1,7 +1,13 @@
 package br.com.alura.screenmatch.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SeriesController {
+
+    @GetMapping("/series")
+    public String getSeries() {
+        return "Series will be listed here.";
+    }
 }
