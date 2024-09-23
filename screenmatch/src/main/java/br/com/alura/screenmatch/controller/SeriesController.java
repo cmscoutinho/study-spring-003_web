c3c1641f6f2f3245e79305278455917615cc0589
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-22 23:47:04 -0300

    refactor: add dependency injection on SeriesControleler

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
@@ -1,10 +1,15 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SeriesController {
+
+    @Autowired
+    SerieRepository repository;
 
     @GetMapping("/series")
     public String getSeries() {
