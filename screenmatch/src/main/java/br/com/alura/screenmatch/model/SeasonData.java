059fe87c8f0c0b57943d31a6f6e18545e3fc9d29
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-25 20:30:02 -0300

    refactor: translate fields of SeasonData to english

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/SeasonData.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/SeasonData.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SeasonData(@JsonAlias("Season") Integer numero,
-                         @JsonAlias("Episodes") List<EpisodeData> episodios) {
+public record SeasonData(@JsonAlias("Title") String title,
+                         @JsonAlias("Season") Integer seasonIdx,
+                         @JsonAlias("totalSeasons") String totalSeasons,
+                         @JsonAlias("Episodes") List<EpisodeData> episodeData) {
 }
