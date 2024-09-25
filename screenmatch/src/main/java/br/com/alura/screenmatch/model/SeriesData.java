7ec9b105a28b7bd882f77337396fe5fc93756442
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-09-25 20:28:44 -0300

    refactor: translate fields of SeriesData to english

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/SeriesData.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/SeriesData.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SeriesData(@JsonAlias("Title") String titulo,
-                         @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") String avaliacao,
-                         @JsonAlias("Genre") String genero,
-                         @JsonAlias("Actors") String atores,
+public record SeriesData(@JsonAlias("Title") String title,
+                         @JsonAlias("Genre") String genre,
+                         @JsonAlias("totalSeasons") Integer seasons,
+                         @JsonAlias("Actors") String actors,
+                         @JsonAlias("Released") String releaseDate,
+                         @JsonAlias("imdbRating") String rating,
                          @JsonAlias("Poster") String poster,
-                         @JsonAlias("Plot") String sinopse) {
+                         @JsonAlias("Plot") String plot) {
 }
