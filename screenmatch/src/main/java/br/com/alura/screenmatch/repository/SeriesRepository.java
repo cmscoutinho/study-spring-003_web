b9653fed32d72fcd9aeea09a0005f16f64d0ad70
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-10-02 01:20:45 -0300

    refactor: adjust SeriesRepository's method names

--- screenmatch/src/main/java/br/com/alura/screenmatch/repository/SeriesRepository.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/repository/SeriesRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SeriesRepository extends JpaRepository<Series, Long> {
-    Optional<Series> findByTitleContainingIgnoreCase(String seriesName);
+    Optional<Series> findByTitleContainsIgnoreCase(String seriesName);
 
     List<Series> findByActorsContainingIgnoreCaseAndRatingGreaterThanEqual(String actorName, double rating);
 
