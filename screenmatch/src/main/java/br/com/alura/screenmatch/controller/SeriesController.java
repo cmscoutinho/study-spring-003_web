4584ef17dd39115766e0d8a7de9c32e825a66b44
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-10-09 23:28:36 -0300

    style: remove unused imports

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
@@ -1,7 +1,6 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SeriesDTO;
-import br.com.alura.screenmatch.repository.SeriesRepository;
 import br.com.alura.screenmatch.service.SeriesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/series")
