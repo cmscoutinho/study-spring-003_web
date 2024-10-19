d261662eca22d9f8982ef1f54c58b468ece18ddc
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-10-18 21:52:59 -0300

    refactor: make methos getByI() return service call

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/SeriesController.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.dto.SeriesDTO;
 import br.com.alura.screenmatch.service.SeriesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,7 @@ public class SeriesController {
     }
 
     @GetMapping("/{id}")
-    public SeriesDTO getById() {
-
+    public SeriesDTO getById(@PathVariable Long id) {
+        return service.getById(id);
     }
 }
