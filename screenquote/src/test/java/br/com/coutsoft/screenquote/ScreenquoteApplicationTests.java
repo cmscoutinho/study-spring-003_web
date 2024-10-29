c1611a9407bca6ba517e32c770f032030147b8fe
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-10-28 23:50:59 -0300

    feat: initial commit for final project

---
+++ screenquote/src/test/java/br/com/coutsoft/screenquote/ScreenquoteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coutsoft.screenquote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenquoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
