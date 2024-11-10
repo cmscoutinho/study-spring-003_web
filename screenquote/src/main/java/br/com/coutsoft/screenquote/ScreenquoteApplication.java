c3996012d8126159a72968c65bd8b2665b4e929d
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-09 23:20:09 -0300

    feat: place initial project generated by the spring initializer

---
+++ screenquote/src/main/java/br/com/coutsoft/screenquote/ScreenquoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.coutsoft.screenquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenquoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenquoteApplication.class, args);
+	}
+
+}
