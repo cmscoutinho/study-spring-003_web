dd2ac666f3312ce62d6ea3fe880cb4572d4dbff8
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-12 11:27:57 -0300

    feat: define autowired QuoteService field

--- screenquote/src/main/java/br/com/coutsoft/screenquote/controller/QuoteController.java
+++ screenquote/src/main/java/br/com/coutsoft/screenquote/controller/QuoteController.java
@@ -1,10 +1,14 @@
 package br.com.coutsoft.screenquote.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuoteController {
+
+    @Autowired
+    private QuoteService service;
 
     @GetMapping("/series/quotes")
     public QuoteDTO getRandomQuote() {
