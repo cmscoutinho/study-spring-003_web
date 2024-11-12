fedfd1318e0de084d5c1a0fb3cba15a96f6bf85a
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-12 11:27:17 -0300

    feat: create method for getting random quote

--- screenquote/src/main/java/br/com/coutsoft/screenquote/controller/QuoteController.java
+++ screenquote/src/main/java/br/com/coutsoft/screenquote/controller/QuoteController.java
@@ -1,4 +1,13 @@
 package br.com.coutsoft.screenquote.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class QuoteController {
+
+    @GetMapping("/series/quotes")
+    public QuoteDTO getRandomQuote() {
+        return service.getRandomQuote();
+    }
 }
