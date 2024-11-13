dba0d9f35348297fb854ea130d19a28520fef83a
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-13 09:53:05 -0300

    refactor: define repository on QuoteService

--- screenquote/src/main/java/br/com/coutsoft/screenquote/service/QuoteService.java
+++ screenquote/src/main/java/br/com/coutsoft/screenquote/service/QuoteService.java
@@ -1,8 +1,14 @@
 package br.com.coutsoft.screenquote.service;
 
 import br.com.coutsoft.screenquote.dto.QuoteDTO;
+import br.com.coutsoft.screenquote.repository.QuoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class QuoteService {
+
+    @Autowired
+    private QuoteRepository repository;
+
     public QuoteDTO getRandomQuote() {
 
     }
