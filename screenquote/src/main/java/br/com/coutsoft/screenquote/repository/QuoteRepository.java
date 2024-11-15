959b87ddc4919698eb469651ee84c402f4636569
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-15 09:19:28 -0300

    refactor: add JPQL annotation to request method on repository

--- screenquote/src/main/java/br/com/coutsoft/screenquote/repository/QuoteRepository.java
+++ screenquote/src/main/java/br/com/coutsoft/screenquote/repository/QuoteRepository.java
@@ -2,8 +2,10 @@ package br.com.coutsoft.screenquote.repository;
 
 import br.com.coutsoft.screenquote.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
+    @Query("SELECT q FROM quote SORT BY function('Random') LIMIT 1")
     public Quote getRandomQuote();
 }
