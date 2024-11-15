b5b99f94c5a48e7d6561d7d50f5e959a7144fbfb
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-15 09:12:49 -0300

    refactor: minor fix on CorsConfiguration

--- screenquote/src/main/java/br/com/coutsoft/screenquote/cors/CorsConfiguration.java
+++ screenquote/src/main/java/br/com/coutsoft/screenquote/cors/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package br.com.coutsoft.screenquote.cors;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
